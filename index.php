03684219d41a3a69340779cd592071464e40e0c4
Rodolfo Puig <Rodolfo.Puig@nbcuni.com>
2012-11-07 11:32:28 -0500

    Adding support for downloading the latest application archive

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once 'vendors/limonade/lib/limonade.php';
 dispatch('/check/:application', 'check_index');
 function check_index($application = NULL) {
     $url = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']).'/data/'.$application;
-    $items = array();
     try {
         $directory = dirname(__FILE__).'/data/'.$application;
         if ($handle = opendir($directory)) {
@@ -31,14 +30,51 @@ function check_index($application = NULL) {
                     }
                 }
             }
+
+            set('application', $application);
+            set('items', $items);
+            set('canonical', $url);
+
+            return html('check.xml.php', 'layout.xml.php');
+        } else {
+            halt(NOT_FOUND, "The application you have requested doesn't exists.");
         }
         arsort($items);
     } catch(Exception $e) {}
+}
 
-    set('application', $application);
-    set('items', $items);
-    set('canonical', $url);
-    return html('check.xml.php', 'layout.xml.php');
+dispatch('/download/:application', 'download_index');
+function download_index($application = NULL) {
+    $url = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']).'/data/'.$application;
+    try {
+        $directory = dirname(__FILE__).'/data/'.$application;
+        if ($handle = opendir($directory)) {
+            while (FALSE !== ($entry = readdir($handle))) {
+                if ($entry != "." && $entry != "..") {
+                    $basename = realpath($directory.'/'.$entry);
+                    $fileinfo = new SplFileInfo($basename);
+                    if (is_dir($basename)) {
+                        if (preg_match('@^[0-9]+\.[0-9]+\.[0-9]+$@i', $entry)) {
+                            $baseurl  = $url.'/'.$entry;
+                            $metadata = json_decode(file_get_contents($basename.'/metadata.json'), TRUE);
+                            $filename  = $metadata['archive'];
+                            $archive = $basename.'/'.$filename;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            header('Content-disposition: attachment; filename='.$filename);
+            header("Content-Type: application/octect-stream");
+            header("Cache-Control: no-cache, private");
+            header("Edge-control: no-store");
+
+            return render_file($archive);
+        } else {
+            halt(NOT_FOUND, "The application you have requested doesn't exists.");
+        }
+    } catch(Exception $e) {}
 }
 
 run();
